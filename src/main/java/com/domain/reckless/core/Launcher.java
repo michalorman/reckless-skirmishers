a30946205b4c56c13e716b5a2b81ff13deb1c79a
Michal Orman <michal.orman@gmail.com>
2012-03-29 21:37:09 +0200

    Introduced buildPackage gradle task.

---
+++ src/main/java/com/domain/reckless/core/Launcher.java
@@ -0,0 +1,9 @@
+package com.domain.reckless.core;
+
+// TODO: this is temporal launcher, need to be replaced with some valid
+// implementation
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Hi there!");
+    }
+}
---
+++ src/main/script/reckless-skirmishers.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+java -jar lib/reckless-skirmishers.jar
