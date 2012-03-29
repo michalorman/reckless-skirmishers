5770229f1108241897f0e312aa59a0212db058a0
Michal Orman <michal.orman@gmail.com>
2012-03-29 23:03:28 +0200

    Modified gradle's jar task to produce fat-jar.

--- src/main/java/com/domain/reckless/core/Launcher.java
+++ src/main/java/com/domain/reckless/core/Launcher.java
@@ -1,9 +1,14 @@
 package com.domain.reckless.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 // TODO: this is temporal launcher, need to be replaced with some valid
 // implementation
 public class Launcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Launcher.class);
+
     public static void main(String[] args) {
-        System.out.println("Hi there!");
+        LOGGER.info("Launching Reckless Skirmishers...");
     }
 }
