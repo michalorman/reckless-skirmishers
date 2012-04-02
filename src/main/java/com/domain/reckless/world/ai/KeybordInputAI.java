051b2eae98cc685f89f085b27f12cb8156f33f58
Tomasz Wojcik <wojcik.tomek@gmail.com>
2012-04-02 19:15:47 +0200

    KeyboardAI now correctly moves diagonally.

--- src/main/java/com/domain/reckless/world/ai/KeybordInputAI.java
+++ src/main/java/com/domain/reckless/world/ai/KeybordInputAI.java
@@ -12,20 +12,27 @@ public class KeybordInputAI implements AI<Player> {
     public void nextMove(Player player, GameContext context) {
         Keyboard keyboard = context.getKeyboard();
         keyboard.update();
+        double deltaX = player.delta.x;
+        double deltaY = player.delta.y;
+        if (isMovingDiagonally(keyboard)) {
+            double deltaMean = (deltaX + deltaY) / 2;
+            deltaX = deltaMean * Math.sqrt(2) / 2;
+            deltaY = deltaMean * Math.sqrt(2) / 2;
+        }
         if (keyboard.isKeyPressed(KeyEvent.VK_LEFT)) {
-            player.pos.x -= player.delta.x;
+            player.pos.x -= deltaX;
             player.facing = 3;
         }
         if (keyboard.isKeyPressed(KeyEvent.VK_RIGHT)) {
-            player.pos.x += player.delta.x;
+            player.pos.x += deltaX;
             player.facing = 6;
         }
         if (keyboard.isKeyPressed(KeyEvent.VK_DOWN)) {
-            player.pos.y += player.delta.y;
+            player.pos.y += deltaY;
             player.facing = 0;
         }
         if (keyboard.isKeyPressed(KeyEvent.VK_UP)) {
-            player.pos.y -= player.delta.y;
+            player.pos.y -= deltaY;
             player.facing = 4;
         }
         if (keyboard.isKeyPressed(KeyEvent.VK_C)) {
@@ -34,5 +41,10 @@ public class KeybordInputAI implements AI<Player> {
         if (keyboard.isKeyPressed(KeyEvent.VK_S)) {
             player.ai = new StunAI(player.ai, 3000);
         }
+    }
+
+    private boolean isMovingDiagonally(Keyboard keyboard) {
+        return (keyboard.isKeyPressed(KeyEvent.VK_LEFT) || keyboard.isKeyPressed(KeyEvent.VK_RIGHT))
+                && (keyboard.isKeyPressed(KeyEvent.VK_UP) || keyboard.isKeyPressed(KeyEvent.VK_DOWN));
     }
 }
